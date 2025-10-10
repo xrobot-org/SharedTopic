13949d87e333d365fe0093c8044d32b450829549
Jiu-xiao <2592509183@qq.com>
2025-10-10 22:22:24 +0800

    Support domain-scoped topics via TopicConfig; replace topic_name with topic_configs.

--- SharedTopic.hpp
+++ SharedTopic.hpp
@@ -7,9 +7,9 @@ constructor_args:
   - uart_name: "usart1"
   - task_stack_depth: 2048
   - buffer_size: 256
-  - topic_name:
+  - topic_configs:
     - "topic1"
-    - "topic2"
+    - ["topic2", "libxr_def_domain"]
 template_args: []
 required_hardware: uart_name, ramfs
 depends: []
@@ -28,20 +28,34 @@ depends: []
 #include "thread.hpp"
 #include "uart.hpp"
 
-class SharedTopic : public LibXR::Application {
+class SharedTopic : public LibXR::Application
+{
  public:
+  struct TopicConfig
+  {
+    const char *name;
+    const char *domain = "libxr_def_domain";
+
+    TopicConfig(const char *name) : name(name) {}
+
+    TopicConfig(const char *name, const char *domain) : name(name), domain(domain) {}
+  };
+
   SharedTopic(LibXR::HardwareContainer &hw, LibXR::ApplicationManager &app,
-              const char *uart_name, uint32_t task_stack_depth,
-              uint32_t buffer_size,
-              std::initializer_list<const char *> topic_names)
+              const char *uart_name, uint32_t task_stack_depth, uint32_t buffer_size,
+              std::initializer_list<TopicConfig> topic_configs)
       : uart_(hw.template Find<LibXR::UART>(uart_name)),
         server_(buffer_size),
         rx_buffer_(new uint8_t[buffer_size], buffer_size),
-        cmd_file_(LibXR::RamFS::CreateFile("shared_topic", CommandFunc, this)) {
-    for (const char *name : topic_names) {
-      auto topic = LibXR::Topic::Find(name);
-      if (topic == nullptr) {
-        XR_LOG_ERROR("Topic not found: %s", name);
+        cmd_file_(LibXR::RamFS::CreateFile("shared_topic", CommandFunc, this))
+  {
+    for (auto config : topic_configs)
+    {
+      auto domain = LibXR::Topic::Domain(config.domain);
+      auto topic = LibXR::Topic::Find(config.name, &domain);
+      if (topic == nullptr)
+      {
+        XR_LOG_ERROR("Topic not found: %s/%s", config.domain, config.name);
         ASSERT(false);
       }
       server_.Register(topic);
@@ -49,23 +63,25 @@ class SharedTopic : public LibXR::Application {
 
     hw.template FindOrExit<LibXR::RamFS>({"ramfs"})->Add(cmd_file_);
 
-    rx_thread_.Create(this, RxThreadFun, "SharedTopic::RxThread",
-                      task_stack_depth, LibXR::Thread::Priority::REALTIME);
+    rx_thread_.Create(this, RxThreadFun, "SharedTopic::RxThread", task_stack_depth,
+                      LibXR::Thread::Priority::REALTIME);
 
     app.Register(*this);
   }
 
-  static void RxThreadFun(SharedTopic *self) {
+  static void RxThreadFun(SharedTopic *self)
+  {
     LibXR::Semaphore sem;
     LibXR::ReadOperation op(sem);
-    while (true) {
-      auto size = LibXR::max(
-          sizeof(LibXR::Topic::PackedDataHeader),
-          LibXR::min(self->uart_->read_port_->Size(), self->rx_buffer_.size_));
-      auto ans = self->uart_->Read(
-          LibXR::RawData{self->rx_buffer_.addr_, size}, op);
+    while (true)
+    {
+      auto size =
+          LibXR::max(sizeof(LibXR::Topic::PackedDataHeader),
+                     LibXR::min(self->uart_->read_port_->Size(), self->rx_buffer_.size_));
+      auto ans = self->uart_->Read(LibXR::RawData{self->rx_buffer_.addr_, size}, op);
 
-      if (ans == ErrorCode::OK) {
+      if (ans == ErrorCode::OK)
+      {
         self->server_.ParseData(LibXR::RawData{self->rx_buffer_.addr_, size});
       }
 
@@ -75,27 +91,34 @@ class SharedTopic : public LibXR::Application {
 
   void OnMonitor() override {}
 
-  static int CommandFunc(SharedTopic *self, int argc, char **argv) {
-    if (argc == 1) {
+  static int CommandFunc(SharedTopic *self, int argc, char **argv)
+  {
+    if (argc == 1)
+    {
       LibXR::STDIO::Printf("Usage:\r\n");
-      LibXR::STDIO::Printf(
-          "  monitor [time_ms] [interval_ms] - test received speed\r\n");
+      LibXR::STDIO::Printf("  monitor [time_ms] [interval_ms] - test received speed\r\n");
       return 0;
-    } else if (argc == 4) {
-      if (strcmp(argv[1], "monitor") == 0) {
+    }
+    else if (argc == 4)
+    {
+      if (strcmp(argv[1], "monitor") == 0)
+      {
         int time = atoi(argv[2]);
         int delay = atoi(argv[3]);
         auto start = self->rx_count_;
-        while (time > 0) {
+        while (time > 0)
+        {
           LibXR::Thread::Sleep(delay);
           LibXR::STDIO::Printf(
-              "%f Mbps\r\n", static_cast<float>(self->rx_count_ - start) * 8.0 /
-                                 1024.0 / 1024.0 / delay * 1000.0);
+              "%f Mbps\r\n", static_cast<float>(self->rx_count_ - start) * 8.0 / 1024.0 /
+                                 1024.0 / delay * 1000.0);
           time -= delay;
           start = self->rx_count_;
         }
       }
-    } else {
+    }
+    else
+    {
       LibXR::STDIO::Printf("Error: Invalid arguments.\r\n");
       return -1;
     }
